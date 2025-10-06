c11fadc1aa98b3b8ca6432860816d8a187d9bd1a
fabiokafka <fabiokafka@example.com>
2025-10-06 11:27:36 -0400

    Implementa novo layout da tela de login

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-cd /www/wwwroot/melina.maremar.tur.br
-source venv/bin/activate
+cd $(dirname "$0")
+source .venv/bin/activate
 export FLASK_APP=src/main.py
 export FLASK_ENV=production
 gunicorn -w 4 -b 0.0.0.0:5000 src.main:app
+
